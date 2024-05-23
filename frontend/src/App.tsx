889ce4426a72ea58189cd5bc7f6b63fdab21c335
JaY4165 <harsha.gontu@gmail.com>
2024-05-23 23:35:04 +0530

    vite config for tanstack router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@radix-ui/themes";
+import { useTheme } from "next-themes";
+
 function App() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <>
-      <h1 className="">hello</h1>
+      <h1>Current theme {theme}</h1>
+      <Button onClick={() => setTheme("dark")}>dark</Button>
+      <Button onClick={() => setTheme("light")}>light</Button>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./css/index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import { ThemeProvider } from "next-themes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <Theme>
+        <App />
+        <ThemePanel />
+      </Theme>
+    </ThemeProvider>
   </React.StrictMode>
 );
