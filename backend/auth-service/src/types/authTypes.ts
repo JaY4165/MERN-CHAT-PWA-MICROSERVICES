ca484052694c9470791c39b35283c470e88ce897
JaY4165 <harsha.gontu@gmail.com>
2024-05-29 16:23:26 +0530

    req validations

--- backend/auth-service/src/types/authTypes.ts
+++ backend/auth-service/src/types/authTypes.ts
@@ -1,7 +1,22 @@
-export interface User {
-    email: string;
-    password: string;
-}
+import z from "zod";
+
+export const UserSchema = z
+    .object({
+        email: z.string().email(),
+        password: z
+            .string()
+            .trim()
+            .min(8, { message: "Password must contain at least 8 characters." })
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                {
+                    message:
+                        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+                }
+            ),
+    });
+
+export type User = z.infer<typeof UserSchema>
 
 export interface SignupResponse {
     id: string;
