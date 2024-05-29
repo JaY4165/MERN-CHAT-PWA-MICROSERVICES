e00d8ba59e8580be8acd44f4dddea38a1b494e36
JaY4165 <harsha.gontu@gmail.com>
2024-05-29 16:35:31 +0530

    req validations success

--- backend/auth-service/src/routes/auth.router.ts
+++ backend/auth-service/src/routes/auth.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { signUp, login, getUser, refreshToken } from "../controllers/auth.controller";
 import { verifyUser } from "../middleware/authMiddleware";
+import { validateRequest } from "../validations/requestValidation";
+import { UserSchema } from "../types/authTypes";
 
 const router: Router = Router();
 
-router.post('/signup', signUp);
+router.post('/signup', validateRequest(UserSchema), signUp);
 
 router.post('/login', login);
 
