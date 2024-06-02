0b92bfe99a3dae7e527a7ede075025daf39ca1fb
JaY4165 <harsha.gontu@gmail.com>
2024-06-03 03:26:34 +0530

    chat route with glassmorphism

--- frontend/src/components/chatComponents/ChatSpace.tsx
+++ frontend/src/components/chatComponents/ChatSpace.tsx
@@ -1,18 +1,77 @@
-import { Box, Grid } from "@radix-ui/themes";
-// import formbg from "/images/formbg"
+import { Box } from "@radix-ui/themes";
+import { cn } from "../../lib/utils";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import lightbg from "/images/chatbg5.jpg";
+import darkbg from "/images/chatbg3.jpg";
 
 const ChatSpace = () => {
+  const [cardColor, setCardColor] = useState<string>("");
+  const darkCard = "bg-[rgba(0,0,0,0.5)]";
+  const lighthCard = "bg-[rgba(255,255,255,0.3)]";
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setCardColor(darkCard);
+    } else {
+      setCardColor(lighthCard);
+    }
+    console.log(cardColor);
+  }, [theme, cardColor]);
+
   return (
-    <main className="p-2">
-      <Grid columns="3" gap="3" rows="repeat(2, 64px)" width="auto">
-        <Box className="bg-red-500" />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-      </Grid>
-    </main>
+    <div className="relative">
+      <img
+        src={theme === "dark" ? darkbg : lightbg}
+        className={cn(
+          "w-full h-[100vh] bg-cover bg-center bg-fixed bg-no-repeat blur-3xl saturate-50 brightness-75  overflow-hidden absolute z-0"
+        )}
+      />
+      <div className="grid p-3 grid-cols-12 w-full gap-3 h-full z-10 fixed">
+        <Box
+          className={cn(
+            "col-span-1 backdrop-blur-xl border-none brightness-110  rounded-lg",
+            cardColor
+          )}
+        ></Box>
+
+        <Box
+          className={cn(
+            "col-span-3 backdrop-blur-xl border-none brightness-110  rounded-lg",
+            cardColor
+          )}
+        >
+          <div className="cursor-pointer">
+            {theme === "dark" ? (
+              <SunIcon
+                className="size-5"
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              />
+            ) : (
+              <MoonIcon
+                className="size-5"
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              />
+            )}
+          </div>
+        </Box>
+        <Box
+          className={cn(
+            "col-span-5 backdrop-blur-xl border-none brightness-110 rounded-lg",
+            cardColor
+          )}
+        ></Box>
+
+        <Box
+          className={cn(
+            "col-span-3 backdrop-blur-xl border-none brightness-110  rounded-lg",
+            cardColor
+          )}
+        ></Box>
+      </div>
+    </div>
   );
 };
 
