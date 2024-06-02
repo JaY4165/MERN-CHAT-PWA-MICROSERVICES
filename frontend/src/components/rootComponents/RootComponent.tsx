da09874106d9b2e8d19d179ffb4e27d9ccfa44bc
JaY4165 <harsha.gontu@gmail.com>
2024-06-03 03:27:03 +0530

    images and backgrounds

---
+++ frontend/src/components/rootComponents/RootComponent.tsx
@@ -0,0 +1,27 @@
+import { Outlet, useLocation } from "@tanstack/react-router";
+import Navbar from "../shared/Navbar";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { useEffect, useState } from "react";
+
+const RootComponent = () => {
+  const match = useLocation();
+  const [chatPathMatched, setChatPathMatched] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (match.pathname.includes("/chat")) {
+      setChatPathMatched(true);
+    } else {
+      setChatPathMatched(false);
+    }
+  }, [match]);
+
+  return (
+    <>
+      {chatPathMatched === false ? <Navbar /> : null}
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+};
+
+export default RootComponent;
