eefb5cda2425101f8c0a5c6af340f0782ce833dd
JaY4165 <harsha.gontu@gmail.com>
2024-06-05 16:08:45 +0530

    user chat ui continues

--- frontend/src/components/chatComponents/ChatSpace.tsx
+++ frontend/src/components/chatComponents/ChatSpace.tsx
@@ -2,7 +2,7 @@ import { Box } from "@radix-ui/themes";
 import { cn } from "../../lib/utils";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import lightbg from "/images/chatbg5.jpg";
+import lightbg from "/images/chatbg-6.jpg";
 import darkbg from "/images/chatbg3.jpg";
 import Sidebar from "./Sidebar";
 import ChatUsers from "./ChatUsers";
@@ -30,17 +30,17 @@ const ChatSpace = () => {
         loading="eager"
         alt="Chat background"
         className={cn(
-          "w-full h-[100vh] bg-cover bg-center bg-fixed bg-no-repeat blur-3xl saturate-50 brightness-75  overflow-hidden absolute z-0"
+          "w-full h-[100vh] bg-cover bg-center bg-fixed bg-no-repeat blur-3xl saturate-100 brightness-75  overflow-hidden absolute z-0"
         )}
       />
-      <div className="grid p-3 grid-cols-12 gap-3 w-full z-10 fixed">
+      <div className="grid p-3 grid-cols-12 gap-3 w-full z-10 fixed ">
         <Box asChild maxHeight={"96vh"}>
           <Sidebar cardColor={cardColor} />
         </Box>
 
         <Box
           className={cn(
-            "col-span-3 backdrop-blur-xl border-none brightness-110 rounded-lg",
+            "col-span-3 backdrop-blur-xl border-none brightness-110  rounded-lg",
             cardColor
           )}
           maxHeight={"96vh"}
--- frontend/src/components/chatComponents/UserChat.tsx
+++ frontend/src/components/chatComponents/UserChat.tsx
@@ -1,8 +1,18 @@
-import { Avatar, Box, Flex, Separator } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Card,
+  Flex,
+  Separator,
+  TextField,
+} from "@radix-ui/themes";
+import { useTheme } from "next-themes";
+import { cn } from "../../lib/utils";
 
 const UserChat = () => {
+  const { theme } = useTheme();
   return (
-    <section className="p-4">
+    <section className="p-4 relative h-full">
       <div>
         <Flex gap="4" align="center">
           <Avatar
@@ -17,7 +27,258 @@ const UserChat = () => {
           </Box>
         </Flex>
         <Separator my="4" size="4" className="opacity-20" />
+        <Box className="w-full space-y-6 overflow-y-scroll max-h-[70vh] no-scrollbar p-2">
+          <div className="flex justify-end gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-2"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">Hi</p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-start gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-0"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Error ratione laboriosam porro omnis nam dicta qui incidunt
+                  odio, vel similique ipsum officiis quae voluptatum labore
+                  architecto perferendis repellendus earum. Dolores.
+                </p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-end gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-2"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">Hi</p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-start gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-0"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Error ratione laboriosam porro omnis nam dicta qui incidunt
+                  odio, vel similique ipsum officiis quae voluptatum labore
+                  architecto perferendis repellendus earum. Dolores.
+                </p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-end gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-2"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">Hi</p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-start gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-0"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Error ratione laboriosam porro omnis nam dicta qui incidunt
+                  odio, vel similique ipsum officiis quae voluptatum labore
+                  architecto perferendis repellendus earum. Dolores.
+                </p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-end gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-2"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">Hi</p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-start gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-0"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Error ratione laboriosam porro omnis nam dicta qui incidunt
+                  odio, vel similique ipsum officiis quae voluptatum labore
+                  architecto perferendis repellendus earum. Dolores.
+                </p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-end gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-2"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">Hi</p>
+              </Card>
+            </div>
+          </div>
+          <div className="flex justify-start gap-3">
+            <Avatar
+              size="1"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+              className="order-0"
+            />
+            <div className=" p-2 rounded-lg">
+              <Card
+                className={cn(
+                  theme === "light"
+                    ? "bg-[rgba(255,255,255,0.3)] text-black"
+                    : "bg-[rgba(21,27,30,0.5)] text-white"
+                )}
+                variant="ghost"
+              >
+                <p className="text-sm break-all text-pretty">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Error ratione laboriosam porro omnis nam dicta qui incidunt
+                  odio, vel similique ipsum officiis quae voluptatum labore
+                  architecto perferendis repellendus earum. Dolores.
+                </p>
+              </Card>
+            </div>
+          </div>
+        </Box>
       </div>
+      <Box className="mt-2 pl-2 absolute top-[89%] w-11/12 max-w-[94%]">
+        {/* <input
+          type="text"
+          placeholder="Type a message"
+          className="w-full p-2 rounded-lg border-none bg-[rgba(21,27,30,0.5)] text-white"
+        /> */}
+        <TextField.Root
+          placeholder="Search for chats"
+          className=""
+          variant="soft"
+          radius="medium"
+          color="mint"
+        ></TextField.Root>
+      </Box>
     </section>
   );
 };
