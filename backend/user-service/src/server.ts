87febbd3317842be7e40f3e76867d65289c513d2
JaY4165 <harsha.gontu@gmail.com>
2024-06-11 22:26:49 +0530

    user service

---
+++ backend/user-service/src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { limiter } from './utils/rate-limiter';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.use(
+    express.json({
+        limit: '1mb',
+    })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    })
+);
+app.use(limiter);
+
+
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`)
+).on('error', (err) => {
+    console.error('Error starting server:', err);
+    process.exit(1);
+});
---
+++ backend/user-service/src/types/env.d.ts
@@ -0,0 +1,15 @@
+/* This code snippet is defining a TypeScript declaration file. */
+// import z from "zod";
+
+// export { };
+// declare global {
+//     namespace NodeJS {
+//         interface ProcessEnv {
+//             PORT: z.ZodNumber;
+//             //   MONGOOSE_URI: string;
+//             NODE_ENV: "test" | "development" | "production";
+//             // JWT_PRIVATE_KEY: string;
+//             // JWT_PUBLIC_KEY: string;
+//         }
+//     }
+// }
